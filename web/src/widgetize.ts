f696718abc04ce32b36b7ad8fa25804721e7c09b
Anders Kaseorg <anders@zulip.com>
2024-11-11 18:59:37 -0800

    web: Add explicit extensions to imports.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/widgetize.ts
+++ web/src/widgetize.ts
@@ -1,11 +1,11 @@
 import $ from "jquery";
 
-import * as blueslip from "./blueslip";
-import type {CallWidgetExtraData, CallWidgetOutboundData} from "./call_widget";
-import * as message_lists from "./message_lists";
-import type {Message} from "./message_store";
-import type {Event, PollWidgetExtraData, PollWidgetOutboundData} from "./poll_widget";
-import type {TodoWidgetOutboundData} from "./todo_widget";
+import * as blueslip from "./blueslip.ts";
+import type {CallWidgetExtraData, CallWidgetOutboundData} from "./call_widget.ts";
+import * as message_lists from "./message_lists.ts";
+import type {Message} from "./message_store.ts";
+import type {Event, PollWidgetExtraData, PollWidgetOutboundData} from "./poll_widget.ts";
+import type {TodoWidgetOutboundData} from "./todo_widget.ts";
 
 // TODO: This ZFormExtraData type should be moved to web/src/zform.js when it will be migrated
 type ZFormExtraData = {
--- web/src/widgets.js
+++ web/src/widgets.js
@@ -1,8 +1,8 @@
-import * as call_widget from "./call_widget";
-import * as poll_widget from "./poll_widget";
-import * as todo_widget from "./todo_widget";
-import * as widgetize from "./widgetize";
-import * as zform from "./zform";
+import * as call_widget from "./call_widget.ts";
+import * as poll_widget from "./poll_widget.ts";
+import * as todo_widget from "./todo_widget.ts";
+import * as widgetize from "./widgetize.ts";
+import * as zform from "./zform.js";
 
 export function initialize() {
     // # Minh: add call widget support
