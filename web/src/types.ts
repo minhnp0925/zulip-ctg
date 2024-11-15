68f47ff71e9520d7b1557dfec113ca8015c7a50e
Sahil Batra <sahil@zulip.com>
2024-11-15 20:16:02 +0530

    state_data: Move group_setting_value_schema to types.ts.
    This is needed to avoid import cycles in next commit.

--- web/src/types.ts
+++ web/src/types.ts
@@ -54,7 +54,9 @@ export type UpdateMessageEvent = {
 
 export type HTMLSelectOneElement = HTMLSelectElement & {type: "select-one"};
 
-export const JitsiMeetExternalAPI_schema = z.object({
-    domain: z.string(),
-    
-})
+export const anonymous_group_schema = z.object({
+    direct_subgroups: z.array(z.number()),
+    direct_members: z.array(z.number()),
+});
+
+export const group_setting_value_schema = z.union([z.number(), anonymous_group_schema]);
